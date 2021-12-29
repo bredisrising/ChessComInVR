0199b1e58cd803d141c55138aae64e4f67b35fbf
bredisrising <ankitmalhotra788@gmail.com>
2021-12-29 03:17:05 -0600

    I dunno

---
+++ CSharpChessBoardGrab/ChessComBoardGrab/ChessboardGrabber.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ChessComBoardGrab
+{
+    class ChessboardGrabber
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver browser;
+            browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://chess.com/");
+        }
+    }
+}
